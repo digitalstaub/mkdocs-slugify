c2cf7d3d660631d650d9c3453505c78f5ecf4326
Benjamin Kotte <kotte@ansit-com.de>
2016-08-24 14:48:27 +0200

    first commit

---
+++ mkdocs-slugify/__init__.py
@@ -0,0 +1,13 @@
+import re
+import unicodedata
+
+def slugify(value, separator):
+
+    """ Slugify a string, to make it URL friendly. """
+    normalized = unicodedata.normalize('NFKD', value)
+    v = normalized.strip().lower()
+    # remove toc numbers
+    v = re.sub('^[]\d\.]+\s?\-?\s?', '', v)
+    # print re.sub('[%s\s]+' % separator, separator, v)
+
+    return re.sub('[%s\s]+' % separator, separator, v)
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mkdocs-slugify',
+      version='0.1',
+      description='slugify removing toc numbers',
+      url='http://github.com/digitalstaub/mkdocs-slugify',
+      author='digitalstaub',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['mkdocs-slugify'],
+      zip_safe=False)
